3d610f8228bec4957b667c9a2cd11fdaf51e6c6f
Steven Hunal <stevehunal@gmail.com>
2019-09-06 19:29:09 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -18,5 +18,20 @@ CREATE TABLE authors(
 CREATE TABLE books (
   id INTEGER PRIMARY KEY
   title TEXT,
-  year 
+  year INTEGER,
+  series_id INTEGER 
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id INTEGER Primary KEy,
+  book_id INTEGER,
+  character_id INTEGER
 );
